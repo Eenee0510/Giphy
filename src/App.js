bdfe29fff5df6596f75931cfaf82ec6432b8425f
mstars_lab2_01 <mstars_lab2_01@mstars-lab2-01s-iMac.local>
2022-03-26 12:59:55 +0800

    Giphy hook

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import axios from "axios";
-import React, { Component } from "react";
 import "./App.css";
 import GifList from './components/GifList'
 import SearchForm from "./components/SearchForm";
+import React, {useState, useEffect} from 'react';
 
+function App() {
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const performSearch = (value) => setQuery(value);
 
-class App extends Component {
-constructor() {
-  super();
-  this.state = {
-    gifs: []
-  }
+  useEffect(() => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=k2wbuloRNTvqpq7Ur2LWB6nebopeIb2F`)
+  .then((response) => setData(response.data.data))
+  .catch((error) =>console.log("Error fetching and parsing data", error))
+  .finally(()=>setIsLoading(false))
+  }, [query]);
+
+  return (
+    <div className="main-header">
+      <div className="inner">
+        <h1 className="main-title">Search App</h1>
+        <SearchForm onSearch = {performSearch}/>
+      </div>
+    <div className="main-content">
+      <GifList data={data} />
+    </div>
+    </div>
+  );
 }
+export default App;
+// class App extends Component {
+// constructor() {
+//   super();
+//   this.state = {
+//     gifs: []
+//   }
+// }
 // componentDidMount() {
 //   const horse = "horse";
 
@@ -25,31 +50,31 @@ constructor() {
 // //   console.log("Error fetching and parsing data", error)
 // // })
 // }
-performSearch = (query) => {
-  axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=k2wbuloRNTvqpq7Ur2LWB6nebopeIb2F`)
-  .then((response) => 
-  {
-    this.setState({
-      gifs: response.data.data,
-    })
-  })
-  .catch((error) => {
-    console.log("Error fetching and parsing data", error);
-  });
-}
- render() {
-    return (
-      <div className="main-header">
-        <div className="inner">
-          <h1 className="main-title">Search App</h1>
-          <SearchForm onSearch = {this.performSearch}/>
-        </div>
-      <div className="main-content">
-        <GifList data={this.state.gifs} />
-      </div>
-      </div>
-    );
-  }
-}
+// performSearch = (query) => {
+//   axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=k2wbuloRNTvqpq7Ur2LWB6nebopeIb2F`)
+//   .then((response) => 
+//   {
+//     this.setState({
+//       gifs: response.data.data,
+//     })
+//   })
+//   .catch((error) => {
+//     console.log("Error fetching and parsing data", error);
+//   });
+// }
+//  render() {
+//     return (
+//       <div className="main-header">
+//         <div className="inner">
+//           <h1 className="main-title">Search App</h1>
+//           <SearchForm onSearch = {this.performSearch}/>
+//         </div>
+//       <div className="main-content">
+//         <GifList data={this.state.gifs} />
+//       </div>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,39 +1,74 @@
-import { Component } from "react";
 
-export default class SearchForm extends Component {
-  state = {
-    searchText: "",
-  }
-  onSearchChange = (e) => {
+import { useState} from "react";
+
+function SearchForm(props) {
+  const [searchText, setSearchText] = useState("")
+  const onSearchChange = (e) => {
    
-  
-    this.setState({searchText: e.target.value});
+    setSearchText(e.target.value)
   }
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     console.log(e)
     e.preventDefault();
-    this.props.onSearch(this.state.searchText)
+    props.onSearch(searchText)
   
     e.currentTarget.reset()
   }
-  render() {
-    return (
+
+  return (
       <div>
-        <form className="search-form" onSubmit={this.handleSubmit}>
-          <label className="is-hidden" htmlFor="search">
-            Search
-          </label>
-          <input type="search" 
-                  name ="search"
+            <form className="search-form" onSubmit={handleSubmit}>
+              <label className="is-hidden" htmlFor="search">
+                 Search
+             </label>
+            <input type="search" 
+                   name ="search"
                 placeholder="Search...."
-                onChange={this.onSearchChange}>
-            </input>
-          <button type="submit"
-                  id="submit"
-                  className="search-button" />
+                onChange={onSearchChange}>
+             </input>
+           <button type="submit"
+                     id="submit"
+                     className="search-button" />
             <i className="material-icons icn-button" />
-        </form>
-      </div>
-    );
-  }
+          </form>
+       </div>
+       );
 }
+// export default class SearchForm extends Component {
+//   state = {
+//     searchText: "",
+//   }
+//   onSearchChange = (e) => {
+   
+  
+//     this.setState({searchText: e.target.value});
+//   }
+//   handleSubmit = (e) => {
+//     console.log(e)
+//     e.preventDefault();
+//     this.props.onSearch(this.state.searchText)
+  
+//     e.currentTarget.reset()
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <form className="search-form" onSubmit={this.handleSubmit}>
+//           <label className="is-hidden" htmlFor="search">
+//             Search
+//           </label>
+//           <input type="search" 
+//                   name ="search"
+//                 placeholder="Search...."
+//                 onChange={this.onSearchChange}>
+//             </input>
+//           <button type="submit"
+//                   id="submit"
+//                   className="search-button" />
+//             <i className="material-icons icn-button" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+export default SearchForm;
